07fdadb1cdcac233dc7214a992913f9f8edaaeb1
NT <nickt444@gmail.com>
2015-11-05 14:08:46 -0500

    fixed io issues

--- app.js
+++ app.js
@@ -1,44 +1,72 @@
 var express = require('express');
 var path = require('path');
-
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var swig = require('swig');
 var routes = require('./server/routes/index');
+var users = require('./server/routes/users');
+
 
 var app = express();
 
-// Configure Express
-var http = require('http').Server(app);
-var io = require('socket.io')(http); // Add socket.io
+//swig setup
+swig.setDefaults({ cache: false });
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-
-//body parser
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Router
 app.use('/', routes);
+app.use('/users', users);
 
-// 404 Middleware
+// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// Sockets
-require('./server/nunchuck-server')(io);
+// error handlers
 
-// -------------------------------
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
 
-// Start the Server listening on 3000
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
+
+
+
 
 module.exports = app;
